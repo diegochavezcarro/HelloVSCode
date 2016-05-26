82ad92e78c7eda05b03ca08905dbbe5bef8335b4
diegochavezcarro <diegochavezcarro@gmail.com>
2016-05-26 16:14:16 -0300

    primera subida

---
+++ app.js
@@ -0,0 +1,13 @@
+var msg = 'hello world';
+msg = 5.5;
+
+var num=5;
+num = '';
+for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+    
+    if (condition) {
+        
+    }
+}
+console.log(msg);
