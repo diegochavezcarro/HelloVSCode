2d6b86466d890f446d3cc5d05770d5357e72d939
diegochavezcarro <diegochavezcarro@gmail.com>
2016-05-26 23:52:20 -0300

    f

--- app.js
+++ app.js
@@ -3,11 +3,4 @@ msg = 5.5;
 
 var num=5;
 num = ' ';
-for (var index = 0; index < array.length; index++) {
-    var element = array[index];
-    
-    if (condition)  {
-        
-    }
-}
 console.log(msg);
